f2e9abd3b77bd51f771f5ba8b561c221bf1db4ee
InfiniteShowrooms (AR headset experiences & stories) <97874276+InfiniteShowrooms@users.noreply.github.com>
2025-06-04 10:03:47 +0200

    Update Qs02 All Business Questions.sql
    -Update question documentation structure

--- Repo Code/Magist-case-study/Qs02 All Business Questions.sql
+++ Repo Code/Magist-case-study/Qs02 All Business Questions.sql
@@ -5,18 +5,51 @@ SELECT * FROM rollup_01_ordered_products_details_w_seller;
 SELECT * FROM rollup_A2_orders_single_line;
 SELECT * FROM rollup_A3_customers_ltv;
 
-
 /*
-2.3. In relation to the delivery time:
+#################################################
+2.1. In relation to the products:
+==================================
+-2.1.1 What categories of tech products does Magist have?
+-2.1.2 How many products of these tech categories have been sold (within the time window of the database snapshot)? What percentage does that represent from the overall number of products sold?
+-2.1.3 What’s the average price of the products being sold?
+-2.1.4 Are expensive tech products popular? (TIP: Look at the function CASE WHEN to accomplish this task)
 */
 
-#What’s the average time between the order being placed and the product being delivered?
+
+
+/*
+#################################################
+2.2. In relation to the sellers:
+==================================
+-2.2.1 How many months of data are included in the magist database?
+-2.2.2 How many sellers are there? How many Tech sellers are there? What percentage of overall sellers are Tech sellers?
+-2.2.3 What is the total amount earned by all sellers? What is the total amount earned by all Tech sellers?
+-2.2.4 Can you work out the average monthly income of all sellers? Can you work out the average monthly income of Tech sellers?
+*/
+
+
+
+/*
+#################################################
+2.3. In relation to the delivery time:
+==================================
+-2.3.1 What’s the average time between the order being placed and the product being delivered?
+-2.3.2 How many orders are delivered on time vs orders delivered with a delay?
+-2.3.3 Is there any pattern for delayed orders, e.g. big products being delayed more often?
+*/
+
+#################################################
+##2.3.1 What’s the average time between the order being placed and the product being delivered?
+#################################################
 SELECT 
     AVG(DATEDIFF(order_delivered_customer_date, order_purchase_timestamp)) AS avg_delivery_days
 FROM rollup_A2_orders_single_line;
 
-##How many orders are delivered on time vs orders delivered with a delay?
 
+#################################################
+##2.3.2 How many orders are delivered on time vs orders delivered with a delay?
+#################################################
+	
 #full list of statuses
 #approved	2 -- INCLUDE: from 2017, took money so why weren't they delivered or cancelled?
 #canceled	625 -- EXCLUDE: they're cancelled
@@ -77,8 +110,9 @@ FROM (
 GROUP BY order_status, delivery_status;
 
 
-
-#Is there any pattern for delayed orders, e.g. big products being delayed more often?
+#################################################
+#-2.3.3 Is there any pattern for delayed orders, e.g. big products being delayed more often?
+#################################################
 SELECT * FROM rollup_01_ordered_products_details_w_seller;
 
 SELECT
